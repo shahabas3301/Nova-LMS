299172e4ab76070cf6642aad1d6ad38a72af0ee6
Nidheesh <nkkpala@gmail.com>
2025-11-19 16:15:05 +0530

    Remove storage folder from git tracking

--- storage/framework/views/399973fa53353c474c3a995ef962034b.php
+++
@@ -1,8 +0,0 @@
-<?php $__env->startSection(config('pagebuilder.site_section')); ?>
-<div class="page-<?php echo e($page->slug == "/" ? "home-page" : $page->slug); ?>">
-    <?php echo $pageSections; ?>
-
-</div>
-<?php $__env->stopSection(); ?>
-
-<?php echo $__env->make(config('pagebuilder.site_layout'),['page' => $page, 'edit' => false ], array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?><?php /**PATH /home/nidheesh/workspace/Nova-LMS/vendor/larabuild/pagebuilder/src/../resources/views/page.blade.php ENDPATH**/ ?>
--- storage/framework/views/3cb30c23a72e4b8f918067b8b44dedee.php
+++
@@ -1,7 +0,0 @@
-<?php $__env->startSection('title', __('general.404_title')); ?>
-<?php $__env->startSection('heading', __('general.went_wrong')); ?>
-<?php $__env->startSection('code', '404'); ?>
-<?php $__env->startSection('img', asset('images/error/404.png')); ?>
-<?php $__env->startSection('message', __('general.went_wrong_dec')); ?>
-
-<?php echo $__env->make('errors::minimal', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?><?php /**PATH /home/nidheesh/workspace/Nova-LMS/resources/views/errors/404.blade.php ENDPATH**/ ?>
